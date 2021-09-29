fac64da3a1dec71313232bc027c8946acf1f2168
Noah Gardner <ngngardner@gmail.com>
2021-09-29 11:11:32 -0400

    drop devshell.toml

--- flake.nix
+++ flake.nix
@@ -21,28 +21,49 @@
         {
           devShell = with pkgs;
             let
-              custom-python-env = pkgs.python3.buildEnv.override
+              python-env = pkgs.python39.buildEnv.override
                 {
-                  extraLibs = with pkgs.python3Packages; [ xlrd ];
+                  extraLibs = with pkgs.python39Packages; [
+                    # extra python packages here
+                  ];
                   ignoreCollisions = true;
                 };
             in
-            with pkgs; devshell.mkShell {
-              imports = [
-                (devshell.importTOML ./devshell.toml)
-              ];
-              
-              env = [
-                {
-                  name = "PYTHON";
-                  value = "${custom-python-env}/bin/python";
-                }
-                {
-                  name = "PYTHONPATH";
-                  value = "${custom-python-env}/${pkgs.python3.sitePackages}";
-                }
-              ];
-            };
+            with pkgs; devshell.mkShell
+              {
+                packages = [
+                  python-env
+                  julia_16-bin
+                ];
+
+                commands = [{
+                  name = "pluto";
+                  category = "Julia";
+                  command = ''
+                    eval $(echo "julia -e 'import Pkg; Pkg.activate(\".\"); using Pluto; Pluto.run()'")
+                  '';
+                  help = "launch pluto server";
+                }];
+
+                env = [
+                  {
+                    name = "PYTHON";
+                    value = "${python-env}/bin/python";
+                  }
+                  {
+                    name = "PYTHONPATH";
+                    value = "${python-env}/${pkgs.python3.sitePackages}";
+                  }
+                  {
+                    name = "JULIA_DEPOT_PATH";
+                    value = "./.julia_depot";
+                  }
+                  {
+                    name = "JULIA_PROJECT";
+                    value = ".";
+                  }
+                ];
+              };
         }
       )
     );
